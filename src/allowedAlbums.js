0b62e43e50c008429b277b03647f3c45927af487
matthewpereira <mail@matthewpereira.com>
2018-06-22 08:44:54 -0400

    Update allowed albums

--- src/allowedAlbums.js
+++ src/allowedAlbums.js
@@ -1,5 +1,56 @@
 const allowedAlbums = [
     '6Hpyr', // base gallery
+    //
+    '9A5me', // May 2013 Snapshots
+    'vDwNE', // Cadillacs and Cadavers at Stones Place
+    'kDk0V', // Rooftop Barbecue at Brian and Melody's
+    '5gxSY', // Barbecue at Morgan and Wesley's - 2013-05-29
+    'EK1TV', // Melody and Brian's barbecue
+    'c6C6p', // Dinner at Wes and Morgan's
+    'uFi37', // OCAD beach party
+    'asLHt', // Wes and Morgan's baby shower
+    'hmwlt', // Innisfil 2013
+    'wV2vZ', // Jasper's baby photos
+    'gpZyA', // Shelbie and Ryan's Birthday Bash
+    'jofpF', // Board Games at Wes and Morgan's
+    'Byn7D', // Festive Friendsday
+    '3F5AU', // Flip Cup Friday
+    'OVK35', // Cottage 2014 Connell and Bray
+    'TPJD5', // Pancake Party at Nicole's
+    'rgEYp', // Friendsday Ribsday
+    'eBf7k', // Friendsday Go Karting
+    'VXBAU', // Innisfil 2014
+    '00gld', // Badlands with Nicole
+    'x88HI', // Shelbie and Ryan's Birthday 2015
+    'fFb08', // Jasper's first birthday 2015
+    '8OXhc', // Toronto Global Game Jam 2015
+    '4p5Kk', // Belleville Trip
+    'SuwHL', // Celeste and Matthew go to Paris
+    'PfzzJ', // Barbecue at Wes and Morgan's
+    'c44Dh', // Austra and Tribe Called Red
+    'l5ON6', // Sandbanks with Nikky and Phil
+    'DNyjC', // Labour day at Hanlan's Point
+    'UmxwY', // Flea Market
+    'crbpP', // Kait and Chris' wedding
+    'as2L5', // Holiday Pictures
+    '7g9Rv', // Matt and Celeste at Dave's Birthday
+    'g6uxa', // Owen at Statflo
+    'q98ao', // Celeste's Grad Party
+    'rOM63', // Barbecue at Brian and Melody's
+    'A0d80', // Toronto Island with Celeste
+    'huRdJ', // Jasper and Olive
+    'P2ysk', // OneEleven Boat Cruise
+    'q5uqT', // Julia's Cats
+    'Lk2aW', // Friendsday Trinity Bellwoods
+    'VZJjm', // Statflo Goodbye Lunch
+    'DDYYq', // Dad's Birthday 2016-09-25
+    '5JQVZ', // Labour Day 2016 at the Cottage
+    'AlXYh', // August Long Weekend at the Cottage 2016
+    'SqJ0r', // Statflo at Mengrai
+    'hjNvg', // Spook Me Like One of Your French Girls (Halloween 2016)
+    'eVOZf', // Halloween 2016 at Wes and Morgan's
+    '01B2c', // Statflo Christmas Party 2016
+    '3pP6c', // Pereira Christmas 2016
     'mlQaJ', // St Lucia
     'ang7R', // Pie day at Statflo
     'WZzKm', // Canada Day with Jess and Alex
@@ -23,10 +74,13 @@ const allowedAlbums = [
     'mUg4z', // Pereira Christmas 2017
     'OMSHY', // Delayed New Years at Shelbie and Ryan's
     'gJzWn', // Lizfest 2018
+    'fC54L', // Augusta and Olive'
     '13FJNN3', // Statflo Potluck 2018
     'SQ3UDDm', // Pereira Easter 2018
     'pTHw6ZO', // Statflo Product Offsite 2018
+    'Z7eHCbx', // Wes and Morgan and the kids at Trinity Bellwoods
     'Dq1xiRO', // Mom's Birthday 2018
+    'zisKD4A', // AB BC Road Trip 2018
 ];
 
 export default allowedAlbums;
