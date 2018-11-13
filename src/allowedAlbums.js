e6afdf972db5b0bd047e34c7214b7a6b55c9c5d2
Matthew Pereira <matthew@statflo.com>
2018-11-13 13:57:41 -0500

    Update allowedAlbums.js

--- src/allowedAlbums.js
+++ src/allowedAlbums.js
@@ -56,7 +56,7 @@ const allowedAlbums = [
     'WZzKm', // Canada Day with Jess and Alex
     'IGnD1', // August Long Weekend 2017
     'OHio2', // Wes Kait Chris Jasper Augusta Jacob
-    'WZzKm', // Statflo Barbecue 2017
+	  'rBfPF', // Statflo Barbecue 2017
     'oKXXB', // The Fix Ice Cream Shop
     '0K45k', // Robin's Birthday 2017
     'nuxA0', // Belleville 2017
