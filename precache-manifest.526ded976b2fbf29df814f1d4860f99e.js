567f60f95a1d4fd2110b9cde9cba8db47d10394c
matthewpereira <mail@matthewpereira.com>
2019-08-15 07:40:20 -0400

    Updates

---
+++ precache-manifest.526ded976b2fbf29df814f1d4860f99e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db954adc562844c694b74425f3597f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7b8e2ec5d12997547d9",
+    "url": "/static/css/main.c2215dcf.chunk.css"
+  },
+  {
+    "revision": "1ad0b619d898bfaf9212",
+    "url": "/static/js/2.ebed44a9.chunk.js"
+  },
+  {
+    "revision": "c7b8e2ec5d12997547d9",
+    "url": "/static/js/main.3a4ec827.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
