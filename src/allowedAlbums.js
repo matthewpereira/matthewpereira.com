5438000f8ab9217bbde962a5d605239d041b60e4
matthewpereira <mail@matthewpereira.com>
2020-01-02 17:59:22 -0500

    Update allowedAlbums.js

--- src/allowedAlbums.js
+++ src/allowedAlbums.js
@@ -2,6 +2,7 @@ const allowedAlbums = {
     "base gallery": '6Hpyr',
     //
     "Connell's 16th Birthday Party": '5h8i28I',
+    "Spring One Platform 2019": 'u8o2fDA',
     "Tobermory and Cyprus Lake Camping": 's23So5d',
     "Pivotal Beach Day 2019": '5Fm0PDU',
     "Pivotal Island 2019": '0877UpO',
