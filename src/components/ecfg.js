bfc9aa5422dcac7856301c03930d008334f26f47
matthewpereira <mail@matthewpereira.com>
2020-05-29 17:22:48 -0400

    emoji and link fixes

---
+++ src/components/ecfg.js
@@ -0,0 +1,11 @@
+const ecfg = {
+    convertShortnames: true,
+    convertUnicode: true,
+    convertAscii: true,
+    style: {
+        height: 24,
+        top: -1,
+    }
+};
+
+export default ecfg;
