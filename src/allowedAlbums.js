92eef5f6699b22a0776df21757adeebc4323592a
Matthew Pereira <mapereira@pivotal.io>
2020-05-29 11:58:37 -0400

    update allowedAlbums.js

--- src/allowedAlbums.js
+++ src/allowedAlbums.js
@@ -1,6 +1,7 @@
 const allowedAlbums = {
     "base gallery": '6Hpyr',
     //
+    "Morocco Highlights": 'vrqIbDB',
     "Essaouira": 'xpQ6wBg',
     "Casablanca": 'RH6bfwp',
     "Tangier": 'JqqzFEE',
