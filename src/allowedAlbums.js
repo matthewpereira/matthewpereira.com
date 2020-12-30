d1744797f98b61e951990740ae5fb60c575c788e
matthewpereira <mail@matthewpereira.com>
2020-12-30 14:35:58 -0500

    Update allowed albums

--- src/allowedAlbums.js
+++ src/allowedAlbums.js
@@ -1,6 +1,7 @@
 const allowedAlbums = {
     "base gallery": '6Hpyr',
     //
+    "Lake Superior 2020": 'exzH2Zt',
     "Lily - 2020/09/26": 'KIdnHx0',
     "Essaouira": 'xpQ6wBg',
     "Casablanca": 'RH6bfwp',
