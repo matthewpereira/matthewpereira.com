d32ea2d42bcc176aa959f2c9e91d28601bb593cf
matthewpereira <mail@matthewpereira.com>
2021-08-02 09:36:24 -0400

    Add mono cliffs

--- src/allowedAlbums.js
+++ src/allowedAlbums.js
@@ -2,6 +2,7 @@ const allowedAlbums = {
     "base gallery": '6Hpyr',
     //
     "Bruce Trail - Family Day": 'fBTCeKF',
+    "Mono Cliffs Park": 'wTrxgrE',
     "Lake Superior 2020": 'exzH2Zt',
     "Lily - 2020/09/26": 'KIdnHx0',
     "Essaouira": 'xpQ6wBg',
