b306ce32f7d3f1d9c10c2447e716525ea773c9c2
Matthew Pereira <mail@matthewpereira.com>
2022-12-04 22:54:39 -0500

    Update authorization

--- src/components/getGalleryImages.jsx
+++ src/components/getGalleryImages.jsx
@@ -1,49 +1,47 @@
-import IMGUR_AUTHORIZATION from '../env';
+import IMGUR_AUTHORIZATION from "../env";
 
-const DEFAULTGALLERY = '6Hpyr';
+const DEFAULTGALLERY = "6Hpyr";
 
 const IN_CASE_OF_ERROR = {
-	"data": {
-		"id": "6Hpyr",
-		"images": [],
-	}
-}
-
-const styleCaptions = albumId => {
-    return (!albumId || albumId === DEFAULTGALLERY) ?
-        'right' :
-        'bottom';
+  data: {
+    id: "6Hpyr",
+    images: [],
+  },
 };
 
-const getGalleryImages = async (albumId) => { 
-    const details = {
-        headers: {
-            'Authorization': IMGUR_AUTHORIZATION
-        }
-    }
+const styleCaptions = (albumId) => {
+  return !albumId || albumId === DEFAULTGALLERY ? "right" : "bottom";
+};
 
-    return await fetch(`https://api.imgur.com/3/album/${albumId}`, details)
-        .then(data => data.json())
-        .then(data => hydrateGalleryState(data))
-        .catch(error => {
-            console.error("Abort, abort!", error);
-            hydrateGalleryState(IN_CASE_OF_ERROR, albumId);
-        });
-}
+const getGalleryImages = async (albumId) => {
+  const details = {
+    headers: {
+      Authorization: `Client-ID: ${IMGUR_AUTHORIZATION.clientId}`,
+    },
+  };
+
+  return await fetch(`https://api.imgur.com/3/album/${albumId}`, details)
+    .then((data) => data.json())
+    .then((data) => hydrateGalleryState(data))
+    .catch((error) => {
+      console.error("Abort, abort!", error);
+      hydrateGalleryState(IN_CASE_OF_ERROR, albumId);
+    });
+};
 
 const hydrateGalleryState = (data, albumId) => {
-    const captions = styleCaptions(data.data.id);
-    const loadedImages = data.data.images;
-    const description = data.data.description || '';
+  const captions = styleCaptions(data.data.id);
+  const loadedImages = data.data.images;
+  const description = data.data.description || "";
 
-    const albumName = data.data.title || 'Matthew Pereira';
+  const albumName = data.data.title || "Matthew Pereira";
 
-    return {
-        albumName,
-        captions,
-        description, 
-        loadedImages, 
-    }
-}
+  return {
+    albumName,
+    captions,
+    description,
+    loadedImages,
+  };
+};
 
 export default getGalleryImages;
